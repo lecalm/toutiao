b86eac5b5c60cca0f36d0970d4d93df46457fd4c
lecalm <xianleupup@icloud.com>
2020-06-29 11:40:58 +0800

    ##实现了项目登录用户的点赞点踩功能

---
+++ src/main/java/com/le/util/RedisKeyUtil.java
@@ -0,0 +1,25 @@
+package com.le.util;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+
+public class RedisKeyUtil {
+    //统一规范redis key的命名方式
+    private static String SPLIT = ":";
+    private static String BIZ_LIKE = "LIKE";
+    private static String BIZ_DISLIKE = "DISLIKE";
+    private static String BIZ_EVENT = "EVENT";
+
+    public static String getEventQueueKey(){
+        return BIZ_EVENT;
+    }
+
+    public static String getLikeKey(int entityType, int entityId){
+        return BIZ_LIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
+    }
+
+    public static String getDisLikeKey(int entityId, int entityType) {
+        return BIZ_DISLIKE + SPLIT + String.valueOf(entityType) + SPLIT + String.valueOf(entityId);
+    }
+}
